b32ccefe622b211cb17231e53e853bace6bd6803
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-10-30 23:50:11 +0100

    Added some comments for future work

--- scripts/keyboardListener.js
+++ scripts/keyboardListener.js
@@ -6,3 +6,6 @@ document.getElementById("calcKeyBoard").addEventListener('click', (event) => {
 
     document.getElementById("calcDisplayInfo").textContent += event.target.innerText;
 })
+
+// perhaps listener can be re-attached based on class - this will require keyboard to simply have it's own
+// class, while giving flexibility to add other buttons to the same div

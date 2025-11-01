1edef2c4674f00d9fb24493af36713cae7d9020c
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-11-02 00:47:34 +0100

    Fixed how dot is added if it was removed

--- scripts/clearOneButtonListener.js
+++ scripts/clearOneButtonListener.js
@@ -1,4 +1,7 @@
-document.getElementById("clearOneBtn").addEventListener('click', (event) => {
+document.getElementById("clearOneBtn").addEventListener('click', () => {
+    if(displayInfo.textContent[displayInfo.textContent.length - 1] == "."){
+        isDot = !isDot;
+    }
     displayInfo.textContent = displayInfo.textContent.slice(0, -1);
 
     if(displayInfo.textContent == "") isDot = !isDot;
